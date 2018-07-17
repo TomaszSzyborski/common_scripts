fb38427db8569db07c93acb787aa94bb08590211
tszyborski <tomasz.szyborski@billennium.com>
2018-07-17 08:23:29 +0200

    fixed generator to list

--- pretty_tables.py
+++ pretty_tables.py
@@ -2,11 +2,17 @@ import itertools
 from tabulate import tabulate
 
 possibilities = ["incorrect","correct","empty"]
-all_options = itertools.product(possibilities, repeat=2)
+all_options = list(itertools.product(possibilities, repeat=2))
 
-table_print_styles = ["plain","simple","grid","fancy_grid","pipe","orgtbl","jira","presto","psql","rst","mediawiki","moinmoin","youtrack","html","latex","latex_raw","latex_booktabs","textile"]
+table_print_styles = ["plain","simple","grid",
+					"fancy_grid","pipe","orgtbl",
+					"jira","presto","psql",
+					"rst","mediawiki","moinmoin",
+					"youtrack","html","latex",
+					"latex_raw","latex_booktabs","textile"]
 
 headers = ["username", "password"]
 
 for print_style in table_print_styles:
-	print(tabulate(possibilities, headers, tablefmt=print_style))
+	print(f"Using style: {print_style}")
+	print(tabulate(all_options, headers, tablefmt=print_style))
