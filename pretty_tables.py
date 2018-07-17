45920d3f98bcc3a5ca7a18702f1c7b6dae0ec21d
tszyborski <tomasz.szyborski@billennium.com>
2018-07-17 08:40:38 +0200

    adding usernames to generate table

--- pretty_tables.py
+++ pretty_tables.py
@@ -2,7 +2,10 @@ import itertools
 from tabulate import tabulate
 
 possibilities = ["incorrect","correct","empty"]
+headers = ["username", "password"]
+
 all_options = list(itertools.product(possibilities, repeat=2))
+all_options = [list(tup) for tup in all_options]
 
 table_print_styles = ["plain","simple","grid",
 					"fancy_grid","pipe","orgtbl",
@@ -11,8 +14,11 @@ table_print_styles = ["plain","simple","grid",
 					"youtrack","html","latex",
 					"latex_raw","latex_booktabs","textile"]
 
-headers = ["username", "password"]
+final_options = []
+for username in ("Admin", "User"):
+	for index, _ in enumerate(all_options):
+		final_options.append([username] + all_options[index])
 
 for print_style in table_print_styles:
 	print(f"Using style: {print_style}")
-	print(tabulate(all_options, headers, tablefmt=print_style))
+	print(tabulate(final_options, headers, tablefmt=print_style))
